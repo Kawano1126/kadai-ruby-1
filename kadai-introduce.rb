ab4114ecb96b81f81f914ecc349b51d1779a1d09
yugo.k <yugon.toworld@gmail.com>
2022-03-26 08:51:07 +0000

    first commit

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,11 +1,7 @@
-name = "河野　雄吾です"
-puts name
-age= "40歳です"
-puts age
-str1 = '河野　雄吾です'
-str2 = '40歳です'
-str = str1 + str2
-puts str
-str = '40'
-num = 
-puts str + num.to_s
+Lastname='河野'
+name='雄吾'
+age=40
+str1=Lastname
+str2=name
+str3=age
+puts str1+str2+"です。"+str3.to_s+"歳です。"
